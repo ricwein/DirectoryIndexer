4bfbaa340fc40fb6462ff4aee9f9f28cb1f854d8
Richard Weinhold <richard.weinhold@check24.de>
2023-11-07 08:24:31 +0100

    fixes remaining stimulus refs

--- config/bundles.php
+++ config/bundles.php
@@ -5,6 +5,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
